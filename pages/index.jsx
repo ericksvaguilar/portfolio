52c28cfbc22bbe7fb0d0e664ec73ff13536c859a
Erick Aguilar <ericksvaguilar@gmail.com>
2021-11-12 20:37:17 -0300

    :wrench: add head to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,13 @@
-const Home = () => <h1>Home</h1>;
+import Head from 'next/head';
+
+const Home = () => (
+	<>
+		<Head>
+			<title>Erick Aguilar</title>
+			<meta name="description" content="Meu site pessoal" />
+		</Head>
+		<h1>Home</h1>
+	</>
+);
 
 export default Home;
