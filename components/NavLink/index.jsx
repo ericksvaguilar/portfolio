d5109adc3491f5879309fca2c6cc066a87e83bdd
Erick Aguilar <ericksvaguilar@gmail.com>
2021-11-13 09:13:11 -0300

    :lipstick: improve styles

--- components/NavLink/index.jsx
+++ components/NavLink/index.jsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 
+import styles from './index.module.scss';
+
 const NavLink = ({ href, name }) => (
 	<Link href={href}>
-		<a>{name}</a>
+		<a className={styles.navlink}>{name}</a>
 	</Link>
 );
 
