75dfebf5ab4ec4b2f9cc51565a117f16df03e778
Erick Aguilar <ericksvaguilar@gmail.com>
2021-11-19 19:31:15 -0300

    :dizzy: selected nav animations

--- components/NavLink/index.jsx
+++ components/NavLink/index.jsx
@@ -2,17 +2,27 @@ import { motion } from 'framer-motion';
 
 import Link from 'next/link';
 
+import { useRouter } from 'next/router';
+
 import styles from './index.module.scss';
 
-const NavLink = ({ href, name }) => (
-	<Link href={href} passHref>
-		<motion.a
-			whileHover={{ backgroundColor: '#272727', color: '#f2f2f2' }}
-			className={styles.navlink}
-		>
-			{name}
-		</motion.a>
-	</Link>
-);
+const NavLink = ({ href, name }) => {
+	const router = useRouter();
+	const isSelected = router.pathname === href;
+
+	return (
+		<Link href={href} passHref>
+			<motion.a
+				whileHover={{ backgroundColor: '#272727', color: '#f2f2f2' }}
+				className={`${styles.navlink} ${isSelected && styles.selected}`}
+			>
+				{name}
+				{isSelected && (
+					<motion.div className={styles.underline} layoutId="underline" />
+				)}
+			</motion.a>
+		</Link>
+	);
+};
 
 export default NavLink;
