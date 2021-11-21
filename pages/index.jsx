96cdd5e20ae0f7685e2be0dfb0db1681fcdc3d8b
Erick Aguilar <ericksvaguilar@gmail.com>
2021-11-21 12:03:32 -0300

    :sparkles: add social media links

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Head from 'next/head';
 
 import styles from 'styles/pages/home.module.scss';
@@ -14,6 +15,30 @@ const Home = () => (
 				<p className={styles.description}>
 					Desenvolvedor Front-end na Docket Brasil.
 				</p>
+				<ul className={styles.linkList}>
+					<li>
+						<motion.a
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.9 }}
+							href="https://github.com/ericksvaguilar"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							GitHub
+						</motion.a>
+					</li>
+					<li>
+						<motion.a
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.9 }}
+							href="https://www.linkedin.com/in/aguilar-erick/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							LinkedIn
+						</motion.a>
+					</li>
+				</ul>
 			</section>
 		</div>
 	</>
